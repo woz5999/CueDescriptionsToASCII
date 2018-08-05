49eff475d23c2de63c3555534b83fc94299450d9
Jeff Wozniak <jeff.wozniak@logicmonitor.com>
2018-08-05 14:33:56 -0500

    refactor for app engine

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker rm -f converter || true &&
 make &&
-docker run -d -p 80:80 --name converter woz5999/cuedescriptionstoascii &&
+docker run -d -p 8080:8080 --name converter woz5999/cuedescriptionstoascii &&
 sleep 2 &&
 docker logs converter
